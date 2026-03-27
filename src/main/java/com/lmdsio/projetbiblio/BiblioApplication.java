17454e93c73c322b1f6309ece73ba2b0c7ec9683
Ilker KAPLAN <ikapl@lmdsio.com>
2026-03-27 09:45:15 +0100

    page6/16 vue welcom

--- src/main/java/com/lmdsio/projetbiblio/BiblioApplication.java
+++ src/main/java/com/lmdsio/projetbiblio/BiblioApplication.java
@@ -3,6 +3,7 @@ package com.lmdsio.projetbiblio;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class BiblioApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(BiblioApplication.class.getResource("views/master.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Biblio 1.0");
+        stage.getIcons().add(new Image(BiblioApplication.class.getResourceAsStream("images/book.png")));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/lmdsio/projetbiblio/MasterController.java
+++ src/main/java/com/lmdsio/projetbiblio/MasterController.java
@@ -1,14 +1,26 @@
 package com.lmdsio.projetbiblio;
 
+import com.lmdsio.projetbiblio.BiblioApplication;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
 
 public class MasterController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ScrollPane contentPane;
+
+    protected void showContent(String fxmlName){
+        Node contentNode;
+        try {
+            FXMLLoader fxmlLoader =
+                    new FXMLLoader(BiblioApplication.class.getResource("views/"+fxmlName+".fxml"));
+            contentNode=fxmlLoader.load();
+        } catch (Exception e) {
+            contentNode=null;
+        }
+        contentPane.setContent(contentNode);
     }
+
 }
